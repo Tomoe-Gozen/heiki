c3952de85dd05170b09ea605b0eee5e68be666d2
Alexandre Le Corre <a.elreco@gmail.com>
2022-01-31 10:16:56 +0100

    Add nextjs

---
+++ lint-staged.config.js
@@ -0,0 +1,12 @@
+// lint-staged.config.js
+module.exports = {
+  // Lint then format TypeScript and JavaScript files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `yarn eslint --fix "${filenames.join('" "')}"`,
+    `yarn prettier --write "${filenames.join('" "')}"`
+  ],
+
+  // Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write "${filenames.join('" "')}"`
+}
