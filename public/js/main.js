3801af8ee7e14be13846cf4f0e01c30969266f95
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-01 00:09:23 +0100

    Better handle cursor animation

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,30 @@
 if (document.querySelector(".mouse-cursor")) {
+  
     const e = document.querySelector('.cursor-inner'),
       t = document.querySelector('.cursor-outer');
-      
-    window.onmousemove = (s) => {
+    let a = document.querySelectorAll('a')
+    const addCursorHover =  () => {
+      e.classList.add('cursor-hover')
+      t.classList.add('cursor-hover')
+    }
+    const removeCursorHover =  () => {
+      e.classList.remove('cursor-hover')
+      t.classList.remove('cursor-hover')
+    }
 
+    window.onmousemove = (s) => {
+      
       t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"
       e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"
       e.style.visibility = "visible"
       t.style.visibility = "visible"
-
-      document.querySelectorAll('a').forEach(item => {
-        item.addEventListener("mouseenter", function () {
-          e.classList.add('cursor-hover'), t.classList.add('cursor-hover')
-        })
-        item.addEventListener("mouseleave", function (item2) {
-          e.classList.remove('cursor-hover'), t.classList.remove('cursor-hover')
-        })
+      
+      a.forEach((elem) => {
+        if (elem.getAttribute('listener') !== 'true') {
+          elem.addEventListener("mouseenter", addCursorHover)
+          elem.addEventListener("mouseleave", removeCursorHover)
+        }
       })
-
     }
 }
 
