25603afd42bfdda8262545a2bb87772110d5787c
Alexandre LE CORRE <a.elreco@gmail.com>
2022-02-06 20:55:17 +0100

    noindex delete

--- pages/_app.js
+++ pages/_app.js
@@ -37,7 +37,6 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="robots" content="noindex, follow" />
         <meta name="description" content="" />
         <meta
           name="viewport"
