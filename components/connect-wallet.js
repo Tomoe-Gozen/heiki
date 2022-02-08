4dd157a17e4946299f059cb0432e5bcec98adcd5
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-08 21:02:20 +0100

    Finished notification

--- components/connect-wallet.js
+++ components/connect-wallet.js
@@ -1,31 +1,59 @@
 import { useWeb3 } from '@3rdweb/hooks'
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import Noty from 'noty'
 
 export default function ConnectWallet() {
-  const { address, connectWallet, chainId, activeProvider, connector } =
-    useWeb3()
+  const { address, connectWallet, chainId } = useWeb3()
 
   const ellipsisString = (str) => {
     return str.substr(0, 5) + '...' + str.substr(str.length - 5, str.length)
   }
 
-  const errorNotification = new Noty({
-    theme: 'mint',
-    text: 'Some notification text'
-  })
-
-  const displayErrorNotification = () => {
-    if (chainId !== 3) {
-      errorNotification.show()
-    }
-  }
   useEffect(() => {
-    displayErrorNotification()
-  })
+    const wallet = localStorage.getItem('wallet')
+      ? JSON.parse(localStorage.getItem('wallet'))
+      : null
+    if (address && chainId) {
+      if (!wallet) {
+        localStorage.setItem(
+          'wallet',
+          JSON.stringify({
+            address,
+            chainId
+          })
+        )
+      } else if (wallet.address != address || wallet.chainId != chainId) {
+        localStorage.setItem(
+          'wallet',
+          JSON.stringify({
+            address,
+            chainId
+          })
+        )
+      }
+    } else if (wallet) {
+      localStorage.removeItem('wallet')
+    }
+  }, [address, chainId])
 
   const clickConnectWallet = async () => {
     await connectWallet('injected')
+
+    if (chainId !== 3) {
+      new Noty({
+        type: 'warning',
+        text: 'You are connected with the Ropsten network',
+        layout: 'top',
+        timeout: 3000
+      }).show()
+    } else {
+      new Noty({
+        type: 'success',
+        text: 'Your wallet is connected via MetaMask',
+        layout: 'top',
+        timeout: 3000
+      }).show()
+    }
   }
 
   return !address ? (
