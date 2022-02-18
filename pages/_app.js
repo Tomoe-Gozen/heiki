62367aa6dcc96a73ead52ee2a0c0ca58d67ac090
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-18 14:09:37 +0100

    add gallery

--- pages/_app.js
+++ pages/_app.js
@@ -93,6 +93,7 @@ function MyApp({ Component, pageProps }) {
 MyApp.getInitialProps = async (appContext) => {
   // calls page's `getInitialProps` and fills `appProps.pageProps`
   const appProps = await App.getInitialProps(appContext)
+  appProps.pageProps.isWhitelist = true
   appProps.pageProps.isWhitelist = await isWhitelisted()
   return { ...appProps }
 }
