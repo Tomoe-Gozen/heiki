abcb0fb30ff4fb5245453f9dd4c1de1544a33f10
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-20 12:37:59 +0100

    test

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,14 +3,14 @@ import { NextRequest, NextResponse } from 'next/server'
 export function middleware(req) {
   const basicAuth = req.headers.get('authorization') || ''
 
-  /* if (basicAuth) {
+  if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
 
     if (user === 'tomoe' && pwd === 'tomoegozennft2022') {
       return NextResponse.next()
     }
-  } */
+  }
 
   return new Response(basicAuth, {
     status: 401,
