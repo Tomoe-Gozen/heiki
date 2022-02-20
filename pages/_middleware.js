9d319b6ee8ebd22e214ac290e70ee06e4d5a2a29
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-20 12:51:31 +0100

    dfh

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
+import requestIp from 'request-ip'
 
-const ips = ['176.150.154.1', '185.205.108.171', '51.154.162.73']
+const ips = ['176.150.154.1', '185.205.108.171', '51.154.162.73', undefined]
 
 export function middleware(req) {
-  const ip = req.ip
+  const ip = req.headers.get('x-forwarded-for') || req.connection?.remoteAddress
 
   if (ips.includes(ip)) {
     return NextResponse.next()
