c01e0623c7ef041031fc1dbfff3ddc9d3f577367
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-20 18:51:09 +0100

    testing

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,12 +1,33 @@
-import { NextRequest, NextResponse } from 'next/server'
-import requestIp from 'request-ip'
-
-const ips = ['176.150.154.1', '185.205.108.171', '51.154.162.73', undefined]
+import { NextResponse } from 'next/server'
 
 export function middleware(req) {
-  NextResponse.next()
-  return
+  if (
+    !process.env.NEXT_PUBLIC_IS_PRODUCTION &&
+    !req.headers.get('host').includes('localhost') &&
+    !req.headers.get('host').includes('127.0.0.1')
+  ) {
+    const basicAuth = req.headers.get('authorization')
 
-  NextResponse.redirect('https://www.tomoegozen.io')
-  return
+    if (basicAuth) {
+      const auth = basicAuth.split(' ')[1]
+      const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+
+      if (user !== 'tomoe' && pwd !== 'tomoegozennft2022') {
+        return new Response('Auth required', {
+          status: 401,
+          headers: {
+            'WWW-Authenticate': 'Basic realm="Secure Area"'
+          }
+        })
+      }
+    }
+
+    return new Response('Auth required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"'
+      }
+    })
+  }
+  return NextResponse.next()
 }
