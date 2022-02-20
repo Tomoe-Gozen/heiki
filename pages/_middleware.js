38c319709dd484608888a1de82f023e7f96614e4
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-20 19:20:35 +0100

    test

---
+++ pages/_middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(req) {
+  if (
+    !process.env.NEXT_PUBLIC_IS_PRODUCTION &&
+    !req.headers.get('host').includes('localhost') &&
+    !req.headers.get('host').includes('127.0.0.1')
+  ) {
+    const basicAuth = req.headers.get('authorization')
+
+    if (basicAuth) {
+      const auth = basicAuth.split(' ')[1]
+      return NextResponse.next()
+      const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+
+      if (user !== 'tomoe' && pwd !== 'tomoegozennft2022') {
+        return new Response('Auth required', {
+          status: 401,
+          headers: {
+            'WWW-Authenticate': 'Basic realm="Secure Area"'
+          }
+        })
+      }
+    } else {
+      return new Response('Auth required', {
+        status: 401,
+        headers: {
+          'WWW-Authenticate': 'Basic realm="Secure Area"'
+        }
+      })
+    }
+  }
+}
