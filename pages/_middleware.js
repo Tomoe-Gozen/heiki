99e30c04277dc441066e64472b10dfa1213d9035
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-20 12:43:19 +0100

    ip auth

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(req) {
-  const basicAuth = req.ip()
+const ips = ['176.150.154.1', '185.205.108.171', '51.154.162.73']
 
-  if (basicAuth) {
+export function middleware(req) {
+  const ip = req.ip
+
+  if (ips.includes(ip)) {
     return NextResponse.next()
   }
 
