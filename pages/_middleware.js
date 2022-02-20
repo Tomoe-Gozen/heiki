0c2134af01d5738d05872c3e536cdf40a75bf1b1
Alexandre LE CORRE <a.elreco@gmail.com>
2022-02-20 11:47:02 +0100

    test

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,8 +1,33 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(req) {
-  if (!process.env.NEXT_PUBLIC_IS_PRODUCTION) {
-    return NextResponse.next()
+  if (
+    !process.env.NEXT_PUBLIC_IS_PRODUCTION &&
+    !req.headers.get('host').includes('localhost') &&
+    !req.headers.get('host').includes('127.0.0.1')
+  ) {
+    // const basicAuth = req.headers.get('authorization')
+
+    // if (basicAuth) {
+    //   const auth = basicAuth.split(' ')[1]
+    //   const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+
+    //   if (user === 'tomoe' && pwd === 'tomoegozennft2022') {
+    //     return NextResponse.next()
+    //   }
+    // }
+
+    return new Response('Auth required', {
+      status: 401,
+      headers: req.headers
+    })
+
+    return new Response('Auth required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"'
+      }
+    })
   }
   return NextResponse.next()
 }
