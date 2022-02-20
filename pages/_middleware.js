4d21532bded6ae1d7d69912bd416ffb7712c4188
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-20 12:30:37 +0100

    check

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,14 +1,13 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(req) {
-  const basicAuth = req.headers.get('authorization')
+  const basicAuth = (req.headers.get('authorization') || '').split(' ')[1] || ''
 
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
-
+    return NextResponse.next()
     if (user === 'tomoe' && pwd === 'tomoegozennft2022') {
-      return NextResponse.next()
     }
   } else {
     return new Response('Auth required', {
