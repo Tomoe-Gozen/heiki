cf6efce9cac7a878f54f2dde24b4636ea5a6e24d
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-20 12:19:56 +0100

    sdg

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,25 +1,21 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(req) {
-  if (!process.env.NEXT_PUBLIC_IS_PRODUCTION) {
-    const basicAuth = req.headers.get('authorization')
+  const basicAuth = req.headers.get('authorization')
 
-    if (basicAuth) {
-      const auth = basicAuth.split(' ')[1]
-      const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+  if (basicAuth) {
+    const auth = basicAuth.split(' ')[1]
+    const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
 
-      if (user === 'tomoe' && pwd === 'tomoegozennft2022') {
-        return NextResponse.next()
-      }
+    if (user === 'tomoe' && pwd === 'tomoegozennft2022') {
+      return NextResponse.next()
     }
-
-    return new Response('Auth required', {
-      status: 401,
-      headers: {
-        'WWW-Authenticate': 'Basic realm="Secure Area"'
-      }
-    })
-  } else {
-    return NextResponse.next()
   }
+
+  return new Response('Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"'
+    }
+  })
 }
