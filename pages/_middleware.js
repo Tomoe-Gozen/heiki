c098c44ac7408364e0c36591586dcb0a8a595ce5
Alexandre LE CORRE <a.elreco@gmail.com>
2022-02-20 11:44:20 +0100

    hj

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,18 +2,7 @@ import { NextResponse } from 'next/server'
 
 export function middleware(req) {
   if (!process.env.NEXT_PUBLIC_IS_PRODUCTION) {
-    const basicAuth = req.headers.get('authorization')
-
-    if (basicAuth) {
-      return NextResponse.next()
-    }
-
-    return new Response('Auth required', {
-      status: 401,
-      headers: {
-        'WWW-Authenticate': 'Basic realm="Secure Area"'
-      }
-    })
+    return NextResponse.next()
   }
   return NextResponse.next()
 }
