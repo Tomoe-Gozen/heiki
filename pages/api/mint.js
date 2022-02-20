132b9503b63597aaed0aa6806b6cfad691e904d8
Alexandre LE CORRE <a.elreco@gmail.com>
2022-02-20 20:44:35 +0100

    test contract

--- pages/api/mint.js
+++ pages/api/mint.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3'
 import getContractObj from './web3/getContract'
 import isWhiteListed from './lib/isWhiteListed'
-import TomoeGozenContract from './contracts/TomoeGozen.json'
+import TomoeGozenContract from './contracts/AlphaTest.json'
 import TomoeGozenContractTest from './contracts/AlphaTest.json'
 
 const mintHandler = async (req, res) => {
