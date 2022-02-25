1bfac0dda85b7c81381f325ebdc20c093f9ee786
Alexandre Le Corre <a.elreco@gmail.com>
2022-02-25 16:44:16 +0100

    Add modal

--- components/nft-modal.js
+++ components/nft-modal.js
@@ -16,12 +16,49 @@ export default function NftModal({ setSelectedNft, selectedNft }) {
     )
     switch (background?.value) {
       case 'Red':
-        return '#B13743'
-      case 'Red':
-        return '#B13743'
-
+      case 'Red Sakura':
+        return '#B23643'
+      case 'White':
+        return '#EFEFEF'
+      case 'White Sakura':
+        return '#B5B4B4'
+      case 'Yellow':
+        return '#FFDC64'
+      case 'Yellow Sakura':
+        return '#E1D072'
+      case 'Army':
+        return '#5C6F60'
+      case 'Dark Green':
+        return '#35513A'
+      case 'Grey':
+        return '#6D6D6D'
+      case 'Dark Grey Arrow':
+        return '#4E4E4E'
+      case 'Blue Kunai':
+        return '#596C8E'
+      case 'Red Fire':
+      case 'Blue Fire':
+        return '#4E4E4E'
+      case 'White Arrow':
+        return '#E5E5E5'
       default:
-        break
+        return ''
+    }
+  }
+
+  const getColor = () => {
+    const background = selectedNft?.attributes.find(
+      (nft) => nft.trait_type === 'Background'
+    )
+    switch (background?.value) {
+      case 'White':
+      case 'Yellow':
+      case 'White Arrow':
+      case 'White Sakura':
+      case 'Yellow Sakura':
+        return 'var(--color-subtitle)'
+      default:
+        return '#EFEFEF'
     }
   }
 
@@ -47,15 +84,35 @@ export default function NftModal({ setSelectedNft, selectedNft }) {
         >
           <div className="d-flex">
             <div>
-              <div className="w-50 h-50">
-                <img
+              <div className="nft-image">
+                <Image
+                  quality="100"
                   src={`https://tomoegozen.ams3.cdn.digitaloceanspaces.com/${selectedNft?.edition}.png`}
-                  className="w-100 h-100"
+                  alt=""
+                  width="512"
+                  height="512"
                 />
               </div>
             </div>
             <div className="p-3">
-              <h3>{selectedNft?.name}</h3>
+              <h3
+                className="m-3 font-tomoe text-lg"
+                style={{ color: getColor() }}
+              >
+                {selectedNft?.name}
+              </h3>
+              <div className="rn-pd-sm-property-wrapper w-100">
+                <div className="property-wrapper property-wrapper-no-wrap">
+                  {selectedNft?.attributes.map((n, index) => (
+                    <div key={index} className="pd-property-inner">
+                      <span className="color-white type">{n.trait_type}</span>
+                      <span className="color-white value">
+                        <strong>{n.value}</strong>
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
