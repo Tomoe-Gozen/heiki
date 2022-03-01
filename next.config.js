04c7774de394a223bb11b8c2405dcf3b5e412648
Alexandre LE CORRE <a.elreco@gmail.com>
2022-03-01 20:33:10 +0100

    redirect mint to home

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/mint',
+        destination: '/',
+        permanent: true
+      }
+    ]
+  }
 }
