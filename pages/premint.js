beb7d06604fb2e59b347139bda1e08ee35001c1d
Alexandre LE CORRE <a.elreco@gmail.com>
2022-04-28 20:13:03 +0200

    better redirect

--- pages/premint.js
+++ pages/premint.js
@@ -1,9 +1,14 @@
-import { useEffect } from 'react'
-
-export default function Premint() {
-  useEffect(() => {
-    window.location.replace('https://www.premint.xyz/heiki-wl-registration')
-  })
-
+const Premint = () => {
   return <div className="p-5">Redirecting to Premint...</div>
 }
+
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: `https://www.premint.xyz/heiki-wl-registration`,
+      permanent: true
+    }
+  }
+}
+
+export default Premint
--- pages/premint/[slug].js
+++ pages/premint/[slug].js
@@ -1,13 +1,21 @@
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-
 const Premint = () => {
-  const router = useRouter()
-  const { slug } = router.query
-  useEffect(() => {
-    window.location.replace(`https://www.premint.xyz/heiki-x-${slug}`)
-  })
   return <div className="p-5">Redirecting to Premint...</div>
+}
+
+export async function getServerSideProps(context) {
+  const slug = context.query.slug
+  if (slug) {
+    return {
+      redirect: {
+        destination: `https://www.premint.xyz/heiki-x-${slug}`,
+        permanent: true
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
 
 export default Premint
