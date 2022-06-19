99acfb4d202f62a9a6c5307ec3ef93fc1ef1b224
Alexandre LE CORRE <a.elreco@gmail.com>
2022-06-19 19:47:58 +0200

    update mint price

--- pages/mint.js
+++ pages/mint.js
@@ -126,7 +126,7 @@ export default function Mint() {
                   {saleFlag === 2 && 'PUBLIC SALE'}
                 </h3>
                 <h4 className="text-center text-secondary font-tomoe text-lg">
-                  Mint price 0.065 eth
+                  Mint price {saleFlag === 1 ? '0.065' : '0.08'} eth
                 </h4>
               </>
             )}
